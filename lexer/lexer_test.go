837127b50201ef72a734ddc83afd2895b26284cd
aymane-smi <aymanebel2@outlook.fr>
2023-06-11 00:02:08 +0100

    adding more test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,20 +8,51 @@ import (
 
 
 func testNextToken(t *testing.T){
-	input := "=+(){},;"
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+	x + y;
+	};
+	let result = add(five, ten);`
 
 	tests := []struct{
 		expectedType token.TokenType
 		expectedLiteral string
 	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.RBRACE, "{"},
-		{token.LBRACE, "}"},
-		{token.COMMA, ","},
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.PLUS, "="},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNC, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.LBRACE, "}"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
