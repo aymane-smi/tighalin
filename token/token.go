3f1c400469638fa01f98eb6dfb5b7ee47008d486
aymane-smi <aymanebel2@outlook.fr>
2023-07-09 19:36:41 +0100

    adding more keywords

--- token/token.go
+++ token/token.go
@@ -43,12 +43,23 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 	CONST    = "CONST"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 // keywords map
 var Keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"const":  CONST,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 //look if the given string is a valid keyword
