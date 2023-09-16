93abdc04b6fe686b3f380d40aec254a2a0afa8b4
aymane-smi <aymanebel2@outlook.fr>
2023-09-16 14:34:15 +0100

    making test valid

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,20 +1,29 @@
 package lexer
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/aymane-smi/tighalin/token"
 )
 
 
-func testNextToken(t *testing.T){
+func TestNextToken(t *testing.T){
 	input := `let five = 5;
 	let ten = 10;
 	let add = fn(x, y) {
 	x + y;
 	};
 	let result = add(five, ten);
-	!*-<5>;`
+	!-/*5
+	5 < 10 > 5;
+	!*-<5>;
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
+	`
 
 	tests := []struct{
 		expectedType token.TokenType
@@ -38,12 +47,14 @@ func testNextToken(t *testing.T){
 		{token.IDENT, "x"},
 		{token.COMMA, ","},
 		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
 		{token.IDENT, "x"},
 		{token.PLUS, "+"},
 		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
-		{token.LBRACE, "}"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
@@ -52,21 +63,52 @@ func testNextToken(t *testing.T){
 		{token.IDENT, "five"},
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTRISK, "*"},
+		{token.INT, "5"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.BANG, "!"},
 		{token.ASTRISK, "*"},
+		{token.MINUS, "-"},
 		{token.LT, "<"},
 		{token.INT, "5"},
 		{token.GT, ">"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
 	l := New(input)
 
 	for i, tt := range tests{
+		fmt.Println(i, tt)
 		tok := l.NextToken()
-
+		fmt.Println(tok.Type, tt.expectedType)
 		if tok.Type != tt.expectedType{
 			t.Fatalf("test[%d] - token Type is not supported. expected %q got %q", i, tt.expectedType, tok.Type)
 		}
