a2fdb0b499eaa6e0fe31177355cb7d8daf62d2ac
belassiria aymane <aymanebel2@outlook.fr>
2023-10-21 18:01:47 +0100

    add first repl version

--- repl/repl.go
+++ repl/repl.go
@@ -1,2 +1,29 @@
 package repl
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/aymane-smi/tighalin/lexer"
+	"github.com/aymane-smi/tighalin/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer, user string) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Println("@" + user + " % " + PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		//%+v\n => print the struct of the tok var with the keys
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
